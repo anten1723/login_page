2eb64d98d798dcd2143a92a4883a7f7d8fab7149
anten cheguevara <anten1723@gmail.com>
2022-01-31 21:22:49 +0000

    initial commit

---
+++ photogram/library/load.php
@@ -0,0 +1,5 @@
+<?
+function load_template($name)
+{
+    include $_SERVER['DOCUMENT_ROOT'] . "/photogram/_template/$name.php";
+}
---
+++ photogram/test.php
@@ -0,0 +1,21 @@
+<pre>
+<?
+
+print("SERVER \n");
+print_r($_SERVER);
+
+print("GET \n");
+print_r($_GET);
+
+print("POST \n");
+print_r($_POST);
+
+print("FILES \n");
+print_r($_FILES);
+
+print("COOKIE \n");
+print_r($_COOKIE);
+
+
+?>
+</pre>
